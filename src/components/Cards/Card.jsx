adb64f7d877e2e28ba10d28d2b8139bca22ec993
Aya1Khlif <ayakhlife222@gmail.com>
2024-03-18 00:01:23 +0300

    edait width the cards

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -6,7 +6,7 @@ export default function Cards({img, title , desc , tophead} ) {
     <div className='container mt-5 ' >
      <div className="">
         <div className="">
-        <Card style={{ width: '23rem' }}>
+        <Card style={{ width: '18rem' }}>
          <Card.Img variant="top" src={img} />
          <Card.Body>
             <span><em>{tophead}</em></span>
